d0865ea1326c5ad1cebdcdb3a34bc102d6c5d0e7
adpatel312 <ankurpatel2015@u.northwestern.edu>
2015-10-20 19:41:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -1,13 +1,6 @@
-var express = require('express');
-var app = express();
-
-app.get('/', function (req, res) {
-  res.send('Hello World!');
-});
-
-var server = app.listen(3000, function () {
-  var host = server.address().address;
-  var port = server.address().port;
-
-  console.log('Example app listening at http://%s:%s', host, port);
-});
+var http = require('http')
+var port = process.env.PORT || 1337;
+http.createServer(function(req, res) {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(port);
