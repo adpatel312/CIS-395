777f3a6fb660410815a31c2704b0ff78227ff473
adpatel312 <ankurpatel2015@u.northwestern.edu>
2015-10-20 21:52:05 -0500

    Update server.js

--- server.js
+++ server.js
@@ -3,4 +3,17 @@ var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
   res.end('File Reader\n');
+  
+  var readline = require('readline');
+
+var high = readline.createInterface({
+input: process.stdin,
+output: process.stdout });
+
+high.question("Enter high number: ", function(answer) {
+console.log("You entered:" , answer);
+
+high.close();
+ });
+
 }).listen(port);
