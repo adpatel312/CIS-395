d5277a5ede7fb0096ef8cdede9d029a0a11c720b
adpatel312 <ankurpatel2015@u.northwestern.edu>
2015-10-20 22:10:49 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
- // prompt("Enter a number")
+  prompt("Enter a number")
   res.end('File Reader\n');
 
 }).listen(port);
