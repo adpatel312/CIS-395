7dcd304b98b2c134698568c8bfb8a2392e218d9d
adpatel312 <ankurpatel2015@u.northwestern.edu>
2015-10-20 22:00:51 -0500

    Update server.js

--- server.js
+++ server.js
@@ -2,18 +2,7 @@ var http = require('http')
 var port = process.env.PORT || 1337;
 http.createServer(function(req, res) {
   res.writeHead(200, { 'Content-Type': 'text/plain' });
-  //res.end('File Reader\n');
-  
-  var readline = require('readline');
-
-var high = readline.createInterface({
-input: process.stdin,
-output: process.stdout });
-
-high.question("Enter high number: ", function(answer) {
-console.log("You entered:" , answer);
-
-high.close();
- });
+  prompt("Enter a number")
+  res.end('File Reader\n');
 
 }).listen(port);
